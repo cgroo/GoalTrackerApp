5bf33ca50f0153f64d886e1c67ce91a69070dc2b
cgroo <glennangelo78@gmail.com>
2024-02-08 23:18:13 +1300

    fix path to start

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 5500; // Use port 5500
 
 // Serve static files from the 'HTML' directory
-app.use(express.static(path.join(__dirname, 'HTML')));
+app.use(express.static(path.join(__dirname, 'goals_app', 'HTML')));
 
 // Log server start message
 server.listen(PORT, () => {
@@ -23,5 +23,5 @@ app.use((req, res, next) => {
 
 // Serve the HTML file when accessing the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'HTML', 'file.html'));
+    res.sendFile(path.join(__dirname, 'goals_app', 'HTML', 'file.html'));
 });
